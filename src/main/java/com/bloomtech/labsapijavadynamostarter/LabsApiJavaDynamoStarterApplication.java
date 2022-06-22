f4413045d23fda1a174c80d66b4ee17c2d06e173
ashtilawat23 <ashtilawat23@gmail.com>
2022-06-22 12:37:40 -0400

    starter app code added

--- src/main/java/com/bloomtech/labsapijavadynamostarter/LabsApiJavaDynamoStarterApplication.java
+++ src/main/java/com/bloomtech/labsapijavadynamostarter/LabsApiJavaDynamoStarterApplication.java
@@ -1,13 +1,16 @@
 package com.bloomtech.labsapijavadynamostarter;
 
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
+@EnableDynamoDBRepositories
 @SpringBootApplication
 public class LabsApiJavaDynamoStarterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LabsApiJavaDynamoStarterApplication.class, args);
     }
-
 }
