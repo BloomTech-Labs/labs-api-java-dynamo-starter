e03bb58b524560df4c384b0873f48fa8e1921ebe
ashtilawat23 <ashtilawat23@gmail.com>
2022-06-22 12:17:48 -0400

    init starter code

---
+++ src/main/java/com/bloomtech/labsapijavadynamostarter/LabsApiJavaDynamoStarterApplication.java
@@ -0,0 +1,13 @@
+package com.bloomtech.labsapijavadynamostarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabsApiJavaDynamoStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabsApiJavaDynamoStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bloomtech/labsapijavadynamostarter/LabsApiJavaDynamoStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloomtech.labsapijavadynamostarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabsApiJavaDynamoStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
